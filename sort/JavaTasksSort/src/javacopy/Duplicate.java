e5ebfb7ddf274baae4649185544f78aaaed09a44
El_Mitya32 <el_mitya@mail.ru>
2019-07-08 13:19:46 +0300

    JavaCopy

--- sort/JavaTasksSort/src/javacopy/Duplicate.java
+++ sort/JavaTasksSort/src/javacopy/Duplicate.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javataskssort;
+package javacopy;
 
 /**
  *
  * @author Asus1
  */
-public class JavaTasksSort {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+class Duplicate {
     
 }
