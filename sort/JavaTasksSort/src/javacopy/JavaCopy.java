7cd852847dc6919e6a0c8968e98d412ecddee85e
El_Mitya32 <el_mitya@mail.ru>
2019-07-09 11:04:00 +0300

    JavaTaskSaper

--- sort/JavaTasksSort/src/javacopy/JavaCopy.java
+++ sort/JavaTasksSort/src/javacopy/JavaCopy.java
@@ -5,17 +5,43 @@
  */
 package javacopy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Asus1
  */
 public class JavaCopy {
+    
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String a[]) {
+        String rus = ".*";
+    int huar = 0;
+        Random r = new Random();
+        String c;
+        Scanner sr = new Scanner(System.in);
+        if (sr.hasNextByte()) {
+            byte aGet = sr.nextByte();
+            byte[] aLength = new byte[aGet];
+            for (byte i = 0; i < aLength.length; i++) {
+                System.out.println();
+                for (byte s = 0; s < aLength.length; s++) {
+                    String sum = rus + rus.toUpperCase();
+                    char cc = sum.charAt(r.nextInt(sum.length()));
+                    if (Character.toString(cc).equals("*")){
+                     huar ++;
+                    }
+                    
+                    System.out.print(cc);
+                }
+                
+            }
+           System.out.println(huar);
+        }
+       
     }
     
 }
