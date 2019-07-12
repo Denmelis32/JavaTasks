6a065e20071da2a8dcc5a44db2db609a3a616dca
El_Mitya32 <el_mitya@mail.ru>
2019-07-12 14:47:04 +0300

    zadanie

--- svjasaniaSpiskov/JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ svjasaniaSpiskov/JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -5,6 +5,10 @@
  */
 package javaapplication9;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Asus1
@@ -14,8 +18,32 @@ public class JavaApplication9 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for(;;){
+        System.out.print("> ");
+       String line = in.readLine();
+        if((line == null) || line.equals("exit"))
+                break;
+        StringBuffer buf = new StringBuffer(line);
+        for(int i = 0; i < buf.length();i++){
+            buf.setCharAt(i, rot13(buf.charAt(i)));
+        }
+        }
+    }
+
+   public static char rot13(char c) {
+       if((c>= 'A')&& (c<= 'Z')){
+        c +=13;
+        if(c >'Z') 
+            c -=26;
+                }
+    if((c>='a')&&(c<='z')){
+        
+        c+=13;
+        if(c>'z') c-=26;
+    }
+    return c ;
     }
     
 }
