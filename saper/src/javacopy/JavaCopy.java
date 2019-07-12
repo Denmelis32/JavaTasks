291fbfe394caffe42dfba2f3cbaa752a4e1c1471
El_Mitya32 <el_mitya@mail.ru>
2019-07-12 18:24:38 +0300

    Было пофикшен баг

--- saper/src/javacopy/JavaCopy.java
+++ saper/src/javacopy/JavaCopy.java
@@ -15,33 +15,36 @@ import java.util.Scanner;
  * @author Asus1
  */
 public class JavaCopy {
-    
 
     public static void main(String a[]) {
+        String sum;
+        Scanner sr = new Scanner(System.in);
+        byte aGet = sr.nextByte();
+        byte aGet2 = sr.nextByte();
         String rus = ".*";
-    int huar = 0;
+        int huar = 0;
+        byte k;
+        int num;
         Random r = new Random();
         String c;
-        Scanner sr = new Scanner(System.in);
-        if (sr.hasNextByte()) {
-            byte aGet = sr.nextByte();
-            byte[] aLength = new byte[aGet];
-            for (byte i = 0; i < aLength.length; i++) {
-                System.out.println();
-                for (byte s = 0; s < aLength.length; s++) {
-                    String sum = rus + rus.toUpperCase();
-                    char cc = sum.charAt(r.nextInt(sum.length()));
-                    if (Character.toString(cc).equals("*")){
-                     huar ++;
+        byte[][] aLength = new byte[aGet][aGet2];
+        byte g = 0;
+        for (byte i = 0; i < aLength.length; i++) {
+            System.out.println();
+            for (byte s = 0; s < aLength[i].length; s++) {
+                sum = rus + rus.toUpperCase();
+                char cc = sum.charAt(r.nextInt(sum.length()));
+                if (Character.toString(cc).equals("*")) {
+
+                    huar++;
+                    if (aLength[i + 1][s + 1] >= 0) {
+
                     }
-                    
+                } else {
                     System.out.print(cc);
                 }
-                
             }
-           System.out.println(huar);
+            System.out.print(huar);
         }
-       
     }
-    
 }
