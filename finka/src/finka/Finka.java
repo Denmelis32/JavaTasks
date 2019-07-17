880962b64ba76e27a769ba9515e855cc594ccc04
El_Mitya32 <el_mitya@mail.ru>
2019-07-17 17:11:40 +0300

    all_Cod

--- finka/src/finka/Finka.java
+++ finka/src/finka/Finka.java
@@ -12,10 +12,26 @@ package finka;
 public class Finka {
 
     /**
+     * 1
+     * Распечатывает 120 первых чисел 
+     * каждый его элемент фор1мируется как сумма двух предыдущих элементов ряда,
+     * начиная с элементов 1 и 1
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       int a = 1;
+       int b = 1;
+       int c ;
+       
+       System.out.println(a+" " + b + " ");
+       for(int i = 0; i<120;i++){
+       c = a+ b;
+       System.out.println(c +" ");
+       b = c ;
+       }
+       System.out.println();
+       
+            
     }
     
 }
