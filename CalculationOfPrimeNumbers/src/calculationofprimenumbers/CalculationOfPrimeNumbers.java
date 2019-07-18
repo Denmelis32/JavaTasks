237de4246b8d9a78ab08d902037bf43aa596226d
El_Mitya32 <el_mitya@mail.ru>
2019-07-18 10:38:03 +0300

    all_cod

--- CalculationOfPrimeNumbers/src/calculationofprimenumbers/CalculationOfPrimeNumbers.java
+++ CalculationOfPrimeNumbers/src/calculationofprimenumbers/CalculationOfPrimeNumbers.java
@@ -12,10 +12,35 @@ package calculationofprimenumbers;
 public class CalculationOfPrimeNumbers {
 
     /**
-     * @param args the command line arguments
+     * эта программа11111 вычисляет простые убрать числа,кратные меньшим простым
+     * числам, и все оставшиеся будут простыми.она печает
+     *
+     * числа не превосходящие аргумент
+     *
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int max = 1000;
+        try {
+            max = Integer.parseInt(args[0]);
+        } catch (Exception e) {
+        }
+        boolean[] a = new boolean[max + 1];
+        for (int i = 0; i <= max; i++) {
+            a[i] = true;
+            a[0] = a[1] = false;
+        }
+        int b = (int) Math.ceil(Math.sqrt(max));
+        for (int i = 0; i <= b; i++) {
+            if (a[i]) {
+                for (int j = 2 * i; j <= max; j = j + 1) {
+                    a[j] = false;
+                }
+            }
+            int r;
+            for (r = max; !a[r]; r--) {
+                System.out.println("Наибольшее число > " + max + " eto" + r);
+            }
+
+        }
     }
-    
 }
