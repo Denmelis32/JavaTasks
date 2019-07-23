364dbb254c6280182169d7232825b39a7ba4e9da
El_Mitya32 <el_mitya@mail.ru>
2019-07-23 15:22:03 +0300

    first commit

---
+++ Journey/src/journey/Journey.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package journey;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus1
+ */
+public class Journey {
+
+    public static void main(String[] args) {
+        int aNumberTraveler;
+        String aTravels;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println(" Куда хотите отправится?" + "\n");
+        for (;;) {
+            if (((aTravels = sc.nextLine()) != null) ) {
+                System.out.println(" Сколько людей с вами отправится? " + "\n");
+                if (((aNumberTraveler = sc.nextInt()) > 0)
+                        && ((aNumberTraveler = sc.nextInt()) < 1000)) {
+                    int aCosts[] = new int[aNumberTraveler];
+                }
+            } else {
+                break;
+            }
+        }
+    }
+}
